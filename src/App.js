b013b4986d2c6fdcac4226b40ba813b06a0aa7b5
AJeannae <93155495+AJeannae@users.noreply.github.com>
2023-01-16 10:48:33 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href="#">Alexandria Marszalek</a> and is{" "}
-        <a
-          href="https://github.com/AJeannae/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://astonishing-sunflower-3f5c0b.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alexandria Marszalek
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AJeannae/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
